a98f3e32a174c81e4746ebda2762e5aaf4154fd9
Ulan <kozhabekov.u@gmail.com>
2023-01-09 00:04:09 +0600

    Initial commit

---
+++ src/main/java/isgp/micro/report/entity/Report.java
@@ -0,0 +1,11 @@
+package isgp.micro.report.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by Ulan on 1/9/2023
+ */
+
+@Document(collection = "report")
+public class Report {
+}
